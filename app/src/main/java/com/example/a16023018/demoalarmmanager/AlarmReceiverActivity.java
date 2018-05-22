c95590e7c92f03fd79bd6b48aa39be52df9f35ec
16023018 <16023018@rp.edu.sg>
2018-05-22 10:39:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16023018/demoalarmmanager/AlarmReceiverActivity.java
@@ -0,0 +1,14 @@
+package com.example.a16023018.demoalarmmanager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+}
